547f23c89c86c9ce277f92055a0476ef889c1adf
Nikolaj Brask-Nielsen <nikolajgive@gmail.com>
2024-05-05 20:28:54 +0200

    feat: Remove old test files

--- src/Nikcio.UHeadless.IntegrationTests/Sqlite/SnapshotConstants.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Nikcio.UHeadless.IntegrationTests.Sqlite;
-
-public static class SnapshotConstants
-{
-    public const string BasePath = "Sqlite/Snapshots";
-}
--- src/Nikcio.UHeadless.IntegrationTests/Sqlite/SqliteConnectionStrings.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Nikcio.UHeadless.IntegrationTests.Sqlite;
-
-public static class SqliteConnectionStrings
-{
-    public static string ConnectionString()
-    {
-        return $"Data Source=|DataDirectory|/Sharable-Test;Cache=Shared;Foreign Keys=True;Pooling=True";
-    }
-}
