c02cc3aa987a29fb2ebbcc482c867651dae0a02c
Nikolaj Brask-Nielsen <nikolajgive@gmail.com>
2024-05-22 21:14:44 +0200

    tests: Utility get claim groups test

---
+++ src/Nikcio.UHeadless.IntegrationTests/ApiAuthTests.UtilityGetClaimGroups.cs
@@ -0,0 +1,45 @@
+using System.Net.Http.Json;
+using Nikcio.UHeadless.IntegrationTests.Defaults;
+
+namespace Nikcio.UHeadless.IntegrationTests;
+public partial class ApiAuthTests
+{
+    private const string _utilityGetClaimGroupsSnapshotPath = $"{SnapshotConstants.AuthBasePath}/UtilityGetClaimGroups";
+
+    [Fact]
+    public async Task Utility_GetClaimGroups_Snaps_Async()
+    {
+        var snapshotProvider = new SnapshotProvider($"{_utilityGetClaimGroupsSnapshotPath}/Snaps");
+        HttpClient client = _factory.CreateClient();
+
+        using var request = JsonContent.Create(new
+        {
+            query = UtilityGetClaimGroupsQueries.GetItems,
+            variables = new
+            {
+            }
+        });
+        HttpResponseMessage response = await client.PostAsync("/graphql", request).ConfigureAwait(true);
+        string responseContent = await response.Content.ReadAsStringAsync().ConfigureAwait(true);
+
+        string snapshotName = $"Utility_GetClaimGroups_Snaps";
+
+        await snapshotProvider.AssertIsSnapshotEqualAsync(snapshotName, responseContent).ConfigureAwait(true);
+        Assert.True(response.IsSuccessStatusCode);
+    }
+}
+
+public static class UtilityGetClaimGroupsQueries
+{
+    public const string GetItems = """
+        query {
+          utility_GetClaimGroups {
+            groupName
+            claimValues {
+              name
+              values
+            }
+          }
+        }
+        """;
+}
