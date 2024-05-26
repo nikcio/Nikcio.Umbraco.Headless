906267ec4b735f5958d13aeeda31a85cb995e59c
Nikolaj Brask-Nielsen <nikolajgive@gmail.com>
2024-05-26 13:04:20 +0200

    chore: Set example as todo

--- examples/code-examples/Headless/UrlTrackerExample/UrlTrackerExampleQuery.cs
+++ examples/code-examples/Headless/UrlTrackerExample/UrlTrackerExampleQuery.cs
@@ -1,31 +1,11 @@
-using HotChocolate.Resolvers;
 using HotChocolate.Types;
 using Nikcio.UHeadless;
-using Nikcio.UHeadless.ContentItems;
 using Nikcio.UHeadless.Defaults.ContentItems;
-using Umbraco.Cms.Core.Models.PublishedContent;
-using UrlTracker.Core;
-using UrlTracker.Web.Processing;
 
 namespace Code.Examples.Headless.UrlTrackerExample;
 
+// TODO
 [ExtendObjectType(typeof(HotChocolateQueryObject))]
 public class UrlTrackerExampleQuery : ContentByRouteQuery
 {
-    protected override ContentItem? CreateContentItem(IPublishedContent? publishedContent, IContentItemRepository<ContentItem> contentItemRepository, IResolverContext resolverContext)
-    {
-        ArgumentNullException.ThrowIfNull(resolverContext);
-        IRequestInterceptFilterCollection requestFilters = resolverContext.Service<IRequestInterceptFilterCollection>();
-        IResponseInterceptHandlerCollection interceptHandlers = resolverContext.Service<IResponseInterceptHandlerCollection>();
-
-        IInterceptService interceptService = resolverContext.Service<IInterceptService>();
-        UrlTracker.Core.Intercepting.Models.IIntercept a = interceptService.GetAsync(UrlTracker.Core.Domain.Models.Url.Parse("https://site.com/home")).Result;
-
-        return base.CreateContentItem(publishedContent, contentItemRepository, resolverContext);
-    }
-
-    protected override Task<ContentItem?> CreateContentItemFromRouteAsync(IResolverContext resolverContext, string route, string baseUrl)
-    {
-        return base.CreateContentItemFromRouteAsync(resolverContext, route, baseUrl);
-    }
 }
